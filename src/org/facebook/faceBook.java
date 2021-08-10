b3ef49d44d2082ec61c334dcf53579d661c91e61
Abhilashdhina <dabhilash161998@gmail.com>
2021-08-10 18:17:43 +0530

    B

--- src/org/facebook/faceBook.java
+++ src/org/facebook/faceBook.java
@@ -1,6 +1,18 @@
 package org.facebook;
 
 public class faceBook {
+	
+	private void hp() {
+		System.out.println("jhjh");
+
+	}
+	 private void dell() {
+		System.out.println("hefdfd");
+
+	}
+	
+	
+	
 	public static void main(String[] args) {
  		
 		
